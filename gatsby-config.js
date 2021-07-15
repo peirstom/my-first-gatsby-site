bd2395051f3a17d8a2506c70eced8b1459dd0e33
Tom Peirs <peirstom@gmail.com>
2021-07-15 14:44:58 +0200

    Preparing a site to go live by adding manifest offline support and meta-data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "My First Gatsby Site",
+    description: `My first gatsby website is about learning gatsby.`,
+    author: `Tom Peirs`,
   },
   plugins: [
     "gatsby-plugin-image",
@@ -14,5 +16,21 @@ module.exports = {
       }
     },
     "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 };
