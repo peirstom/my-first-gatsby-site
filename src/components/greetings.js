a02811ea437f5f58d2ea632095075a480631f927
Tom Peirs <peirstom@gmail.com>
2021-07-15 12:40:26 +0200

    Finished Gatsby Tutorial Part 2

---
+++ src/components/greetings.js
@@ -0,0 +1,11 @@
+// Step 1: Import your component
+import * as React from 'react'
+
+// Defining the <Greeting> component
+const Greeting = (props) => {
+    return (
+      <p>Hi {props.name}!</p>
+    )
+  }
+
+export default Greeting
